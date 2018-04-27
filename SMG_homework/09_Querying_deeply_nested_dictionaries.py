e80be5b2e0ac1094aaa3e2629fff1a51c041b296
Wojciech Minda <w.minda@DIGITAL.local>
2018-04-27 15:36:42 +0200

    homework added

---
+++ SMG_homework/09_Querying_deeply_nested_dictionaries.py
@@ -0,0 +1,88 @@
+'''
+Write a function called fetch() which takes two arguments:
+1. A dictionary. This dictionary may have other nested dictionaries (see example below).
+2. A string which is path of keys in the dictionary. Path components are separated by dots.
+
+The function returns value assigned to the last key on the path.
+
+You may use the following sample deeply nested dictionaries:
+
+SOLAR_SYSTEM = {
+    'planets': {
+        'Mercury': { 'mass': 0.055, 'year': 87.9691 },
+        'Venus': { 'mass': 0.815, 'year': 224.701 },
+        'Mars': {
+            'mass': 0.107,
+            'year': 686.971,
+            'moons': {
+                'Phobos': {'mass': 0.000000001785},
+                'Deimos': {'mass': 0.000000000247}
+            }
+        }
+    },
+    'units': {'mass': 'Earth masses', 'year': 'Earth days'}
+}
+
+In this case, consider this code snippet:
+year = fetch(SOLAR_SYSTEM, 'planets.Venus.year')
+unit = fetch(SOLAR_SYSTEM, 'units.year')
+print(year, unit)  # prints: 224.701 Earth days
+
+The function may ignore any errors resulting from incorrect object types or missing keys:
+fetch(SOLAR_SYSTEM, 'planets.Venus.volume')       # KeyError
+fetch(SOLAR_SYSTEM, 'planets.Venus.year.length')  # TypeError
+
+As an additional challenge, add support for lists. If a path component consists of digits only,
+assume the object being indexed is a list and convert the component to a number.
+isdigit() string method might prove useful.
+'''
+
+
+def fetch(nested_dictionary, path_of_keys):
+    path_of_keys_list = path_of_keys.split('.')
+    for key in path_of_keys_list:
+        if key in nested_dictionary:
+            nested_dictionary = nested_dictionary[key]
+        else:
+            return 'There are no such item!'
+    return nested_dictionary
+
+
+SOLAR_SYSTEM = {
+    'planets': {
+        'Mercury': {
+            'mass': 0.055,
+            'year': 87.9691},
+        'Venus': {
+            'mass': 0.815,
+            'year': 224.701},
+        'Mars': {
+            'mass': 0.107,
+            'year': 686.971,
+            'moons': {
+                'Phobos': {'mass': 0.000000001785},
+                'Deimos': {'mass': 0.000000000247}
+            }
+        }
+    },
+    'units': {
+        'mass': 'Earth masses',
+        'year': 'Earth days'
+    }
+}
+
+mass = fetch(SOLAR_SYSTEM, 'planets.Mercury.mass')
+unit = fetch(SOLAR_SYSTEM, 'units.mass')
+print('Planet mass:', mass, unit)
+
+year = fetch(SOLAR_SYSTEM, 'planets.Venus.year')
+unit = fetch(SOLAR_SYSTEM, 'units.year')
+print('Year duration:', year, unit)
+
+mass = fetch(SOLAR_SYSTEM, 'planets.Mars.moons.Deimos.mass')
+unit = fetch(SOLAR_SYSTEM, 'units.mass')
+print('Moon mass:', mass, unit)
+
+print(fetch(SOLAR_SYSTEM, 'planets.Venus.volume'))
+
+fetch(SOLAR_SYSTEM, 'planets.Venus.year.length')    # TypeError
---
+++ SMG_homework/09_Replacing_words.py
@@ -0,0 +1,39 @@
+'''
+Write replace_words() function which takes two arguments:
+• A text (str) in which some of the words are going to be replaced.
+• A mapping (dict) from words to be replaced (str) to the new replacement words (str).
+The function returns text with all words which appear in the mapping replaced.
+A word is defined as having a single space or the beginning of the string on its left and a single space
+or the end of the string on its right.
+
+Example usage of the function:
+TEXT = "We went to Wellington to find a nice place to eat. We think Wellington is nice!"
+
+modified = replace_words(TEXT, {"Wellington": "Warsaw", "nice": "cool"})
+print(modified)
+# prints: We went to Warsaw to find a cool place to eat. We think Warsaw is nice!
+
+print(replace_words(modified, {"We": "You"}))
+# prints: You went to Warsaw to find a cool place to eat. You think Warsaw is nice!
+
+Note that in the above example TEXT contains words "nice" and "nice!", so only the first one is replaced.
+Just split on a single space!
+'''
+
+def replace_words(to_replace, replacement_dict):
+    new_text = []
+    for word in to_replace.split():
+        for key in replacement_dict:
+            if word == key:
+                word = replacement_dict[key]
+        new_text.append(word)
+    return ' '.join(new_text)
+
+text = "We went to Wellington to find a nice place to eat. We think Wellington is nice!"
+print(text)
+
+replacement_dictionary = {"Wellington": "Warsaw", "nice": "cool"}
+print(replacement_dictionary)
+
+modified = replace_words(text, replacement_dictionary)
+print(modified)
